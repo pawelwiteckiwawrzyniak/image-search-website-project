7e077546c33e5a43c6f5eb5fa001c33d6aac9b10
Paweł Witecki-Wawrzyniak <pawel.witecki.wawrzyniak@gmail.com>
2023-07-16 18:03:49 +0200

    INFINISCROLL Version

--- src/index.js
+++ src/index.js
@@ -6,14 +6,12 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const form = document.querySelector('form');
 const submitBtn = document.querySelector('button[type=submit]');
 const gallery = document.querySelector('.gallery');
-const loadBtn = document.querySelector('button.load-more');
 const lightbox = new SimpleLightbox('.gallery__item', { sourceAttr: 'href' });
 
 let page = 1;
 let perPage = 40;
 let limit = '';
 let numberOfPosts = 40;
-let total = '';
 let searchValue = '';
 
 function fetchPics() {
@@ -26,11 +24,11 @@ function fetchPics() {
     page: page,
     per_page: perPage,
   });
+
   return axios
     .get(`https://pixabay.com/api/?${searchParams}`)
     .then(response => {
       limit = response.data.totalHits;
-      total = response.data.total;
       const imageList = response.data.hits;
       if (imageList.length == 0) {
         return Notiflix.Notify.failure(
@@ -46,7 +44,8 @@ function renderPics(array) {
   const markup = array
     .map(
       ({ webformatURL, largeImageURL, likes, views, comments, downloads }) => {
-        return `<div class="photo-card">
+        return `
+        <div class="photo-card">
         <a href="${largeImageURL}" class="gallery__item"><img src="${webformatURL}" alt="image" loading="lazy"/></a>
         <div class="info">
           <p class="info-item">
@@ -62,15 +61,18 @@ function renderPics(array) {
             <b> Downloads: </b>${downloads}
           </p>
         </div>
-      </div>`;
+      </div>   
+      `;
       }
     )
     .join('');
+
   gallery.insertAdjacentHTML('beforeend', markup);
 }
 
 function handleClickSubmit(e) {
   e.preventDefault();
+
   if (form.searchQuery.value == '') {
     return Notiflix.Notify.info(
       'Type something to get awesome pictures! Pups are always cute!'
@@ -78,72 +80,55 @@ function handleClickSubmit(e) {
   }
   if (searchValue == form.searchQuery.value && searchValue != '') {
     return Notiflix.Notify.info(
-      `For more pictures of a ${searchValue}, scroll down and click "Load more" button`
+      `For more pictures of a ${searchValue}, just scroll down :)`
     );
   }
+
   gallery.innerHTML = '';
-  loadBtn.classList.add('hidden');
   page = 1;
+
   fetchPics().then(imageList => {
     renderPics(imageList);
-    loadBtn.classList.remove('hidden');
     Notiflix.Notify.success(`Hooray! We found ${limit} images.`);
     lightbox.refresh();
     const { height: cardHeight } =
       gallery.firstElementChild.getBoundingClientRect();
-
     window.scrollBy({
       top: cardHeight * 0.4,
       behavior: 'smooth',
     });
   });
+
   searchValue = form.searchQuery.value;
 }
 
-function handleClickLoad(e) {
+function handleLoad(e) {
   form.searchQuery.value = searchValue;
-
   numberOfPosts += perPage;
-  loadBtn.classList.add('hidden');
   page += 1;
 
   if (numberOfPosts >= limit) {
-    perPage = numberOfPosts - limit;
-    fetchPics().then(imageList => {
-      renderPics(imageList);
-      loadBtn.classList.remove('hidden');
-      lightbox.refresh();
-      const { height: cardHeight } =
-        gallery.firstElementChild.getBoundingClientRect();
-
-      window.scrollBy({
-        top: cardHeight * 2.55,
-        behavior: 'smooth',
-      });
-    });
-    loadBtn.removeEventListener('click', handleClickLoad);
-    loadBtn.addEventListener('click', () => {
-      Notiflix.Notify.failure(
-        "We're sorry, but you've reached the end of search results."
-      );
-      loadBtn.classList.add('hidden');
-    });
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+    window.removeEventListener('scroll', handleInfiniteScroll);
     return;
   }
 
   fetchPics().then(imageList => {
     renderPics(imageList);
-    loadBtn.classList.remove('hidden');
     lightbox.refresh();
-    const { height: cardHeight } =
-      gallery.firstElementChild.getBoundingClientRect();
-
-    window.scrollBy({
-      top: cardHeight * 2.55,
-      behavior: 'smooth',
-    });
   });
 }
 
+function handleInfiniteScroll() {
+  const endOfPage =
+    window.innerHeight + window.scrollY >= document.body.offsetHeight;
+
+  if (endOfPage) {
+    handleLoad();
+  }
+}
+
 submitBtn.addEventListener('click', handleClickSubmit);
-loadBtn.addEventListener('click', handleClickLoad);
+window.addEventListener('scroll', handleInfiniteScroll);
